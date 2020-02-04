c614396ca2a7c8a0d5a5c41924899cc97cd834ad
Renato Franco <renato_ny@live.com>
2020-02-03 21:01:02 -0300

    Oauth2 Added

---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,17 @@
+class AuthenticationController < ApplicationController
+	skip_before_action :authenticate_request
+
+	def authenticate
+		command = AuthenticateUser.call(params[:email], params[:password])
+
+		if (command.success?)
+			render json: {
+				auth_token: command.result
+			}, status: 201
+		else
+			render json: {
+				error: command.errors
+			}, status: :unauthorized
+		end
+	end
+end
